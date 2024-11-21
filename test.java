b752b0f0e1b1410837d5c37fe50e8a9eb4e8b5c3
sivakrishna2816 <sivakrishnaeee209@gmail.com>
2024-11-21 16:25:48 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class Test{
     public static void main(string args[]){
        system.out.println("welcome to devops");
-       system.out.println("welcome to devos")
+       system.out.println("welcome to devos");
   }
 }
